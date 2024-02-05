d0bee3693922cd7411022d9d012483770ef125e0
Serhii Mykhailevskyi <serhii.mykhailevskyi@gmail.com>
2024-02-05 11:43:37 +0200

    Implement solution of the remove element

--- js/array_string/remove_element/index.js
+++ js/array_string/remove_element/index.js
@@ -6,5 +6,14 @@
 // Return k.
 
 const removeElement = function(nums, val) {
-    
+  let index = 0;
+
+  for(let i = 0; i < nums.length; i++) {
+    if(nums[i] !== val) {
+      nums[index] == nums[i];
+      index++;
+    }
+  }
+
+  return index;
 };
