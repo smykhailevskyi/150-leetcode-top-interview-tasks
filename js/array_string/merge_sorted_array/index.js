a255b274137e0b2b7ae1f297c3f664bffb7da4fa
Serhii Mykhailevskyi <serhii.mykhailevskyi@gmail.com>
2024-02-05 09:52:37 +0200

    Implement merge sorted array function

--- js/array_string/merge_sorted_array/index.js
+++ js/array_string/merge_sorted_array/index.js
@@ -6,5 +6,18 @@
 // and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
 const merge = function(nums1, m, nums2, n) {
+  let first = m - 1;
+  let second = n - 1;
 
+  for(let i = m + n - 1; i >= 0; i--) {
+    if(second < 0) break;
+    
+    if(nums1[first] > nums2[second]) {
+      nums1[i] = nums1[first];
+      first--;
+    } else {
+      nums1[i] = nums2[second];
+      second--;
+    }
+  }
 }
